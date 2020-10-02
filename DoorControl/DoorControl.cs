bbffdb09f34299f9aa6111cda34de9ab65637811
Thunder <thundermaker300@gmail.com>
2020-10-01 21:28:34 -0400

    Init

---
+++ DoorControl/DoorControl.cs
@@ -0,0 +1,14 @@
+﻿using Exiled.API.Features;
+using Exiled.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControl
+{
+    public class DoorControl : Plugin<Config>
+    {
+    }
+}
